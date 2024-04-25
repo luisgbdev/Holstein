4d54f3d73977ffa41910bd0016df85a3e8d5a103
luis <luissgon100@gmail.com>
2024-04-25 11:58:02 +0200

    Conexión git, estructura, databaseconection

---
+++ M03 Programación/java/g16_holstein/Main.java
@@ -0,0 +1,15 @@
+package g16_holstein;
+
+//import java.sql.SQLException;
+
+public class Main {
+
+	//public static void main(String[] args) throws SQLException{//
+		//System.out.println("Hello World!");//
+     // DatabaseConection db = new DatabaseConection();
+     // db.init();
+      //
+	//}
+
+//}
+}
---
+++ M03 Programación/java/g16_holstein/Test.java
@@ -0,0 +1,35 @@
+package g16_holstein;
+
+public class Test {
+	private int id;
+    private String text;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Test(int id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+    public Test (String text){
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                '}';
+    }
+
+    public String getText() {
+        return text;
+    }
+}
