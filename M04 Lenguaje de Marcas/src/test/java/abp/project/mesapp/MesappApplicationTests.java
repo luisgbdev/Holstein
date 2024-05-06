2192bcfd1486b406e825e5db63e71f76da2e59f8
luiswrld <luissgon100@gmail.com>
2024-05-06 08:28:41 +0000

    Upload New File

---
+++ M04 Lenguaje de Marcas/src/test/java/abp/project/mesapp/MesappApplicationTests.java
@@ -0,0 +1,13 @@
+package abp.project.mesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
