e9f6f5c1937909462572c93d3a09fe00d214dbb5
luis <luissgon100@gmail.com>
2024-05-15 10:43:28 +0200

    errores de expeciones y constantes

---
+++ src/main/java/checkExcepcions/Checkerror.java
@@ -0,0 +1,5 @@
+package checkExcepcions;
+
+public class Checkerror {
+
+}
--- src/main/java/java/functionalities/CamareroFunctionality.java
+++ src/main/java/java/functionalities/CamareroFunctionality.java
@@ -1,4 +1,4 @@
-package functionalities;
+package java.functionalities;
 import java.sql.*;
 import java.util.*;
 public class CamareroFunctionality {
